2c5384dcd5b2e95f4c08a4d2e7af489bae3f5a6a
anhnbt-it <anhnbt.it@gmail.com>
2020-10-27 10:11:33 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: AnhNBT (anhnbt.it@gmail.com)
+ * Date: 10/27/2020
+ * Time: 9:52 AM
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter source file: ");
+        String sourcePath = input.nextLine();
+        System.out.println("Enter destination file: ");
+        String destPath = input.nextLine();
+        File sourceFile = new File(sourcePath);
+        File destFile = new File(destPath);
+
+        try {
+            copyFileUsingJava7Files(sourceFile, destFile);
+            System.out.println("Copy completed.");
+        } catch (IOException e) {
+            System.err.println("Can't copy that file");
+            e.printStackTrace();
+        }
+    }
+
+    private static void copyFileUsingJava7Files(File source, File dest) throws IOException {
+        Files.copy(source.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private static void copyFileUsingStream(File source, File dest) throws IOException {
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        inputStream = new FileInputStream(source);
+        outputStream = new FileOutputStream(dest);
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = inputStream.read(buffer)) > 0) {
+            outputStream.write(buffer, 0, length);
+        }
+        inputStream.close();
+        outputStream.close();
+    }
+}
